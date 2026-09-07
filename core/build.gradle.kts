90bf46c483b674960efd3c82c0aad86ea563c789
kanunal99-jpg <kanunal99@gmail.com>
2026-09-07 22:34:21 +0300

    fix(build): align core Java and Kotlin JVM targets

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -3,8 +3,24 @@ plugins {
     id("org.jetbrains.kotlin.android")
 }
 
-android { namespace = "com.haritalar.core"; compileSdk = 35
-    defaultConfig { minSdk = 26 }
+android {
+    namespace = "com.haritalar.core"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
-dependencies { testImplementation(kotlin("test")) }
+dependencies {
+    testImplementation(kotlin("test"))
+}
