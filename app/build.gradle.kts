e59139a8f1fb682520fb1fba55840f1cffb0983f
kanunal99-jpg <kanunal99@gmail.com>
2026-09-07 21:19:07 +0300

    build: add Android app module

---
+++ app/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.haritalar.app"
+    compileSdk = 35
+    defaultConfig { applicationId = "com.haritalar.app"; minSdk = 26; targetSdk = 35; versionCode = 1; versionName = "0.1.0" }
+}
+
+dependencies { implementation(project(":core")) }
