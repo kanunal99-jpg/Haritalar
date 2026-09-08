09fdf57450757a8a42de870385b56665e98b9fe2
kanunal99-jpg <kanunal99@gmail.com>
2026-09-08 16:05:44 +0300

    test(route): cover composed toll and ferry policies

--- core/src/test/kotlin/com/haritalar/core/navigation/ValhallaRoutePolicyTest.kt
+++ core/src/test/kotlin/com/haritalar/core/navigation/ValhallaRoutePolicyTest.kt
@@ -27,6 +27,29 @@ class ValhallaRoutePolicyTest {
     }
 
     @Test
+    fun combinedNoTollNoFerryPolicySetsBothConstraints() {
+        val options = ValhallaRoutePolicy.autoCostingOptions("no_toll_no_ferry")
+        assertEquals(0.0, options.getDouble("use_tolls"), 0.0)
+        assertEquals(3600.0, options.getDouble("toll_booth_penalty"), 0.0)
+        assertEquals(0.0, options.getDouble("use_ferry"), 0.0)
+    }
+
+    @Test
+    fun combinedTollFastFerryPolicySetsBothPreferences() {
+        val options = ValhallaRoutePolicy.autoCostingOptions("toll_fast_ferry")
+        assertEquals(1.0, options.getDouble("use_tolls"), 0.0)
+        assertEquals(1.0, options.getDouble("use_ferry"), 0.0)
+    }
+
+    @Test
+    fun combinedNoTollFerryPolicyKeepsFerryAvailable() {
+        val options = ValhallaRoutePolicy.autoCostingOptions("no_toll_ferry")
+        assertEquals(0.0, options.getDouble("use_tolls"), 0.0)
+        assertEquals(3600.0, options.getDouble("toll_booth_penalty"), 0.0)
+        assertEquals(1.0, options.getDouble("use_ferry"), 0.0)
+    }
+
+    @Test
     fun explicitFerryManeuverIsDetected() {
         assertTrue(ValhallaRoutePolicy.isFerryManeuver(JSONObject().put("ferry", true)))
         assertTrue(ValhallaRoutePolicy.isFerryManeuver(JSONObject().put("travel_type", "ferry")))
