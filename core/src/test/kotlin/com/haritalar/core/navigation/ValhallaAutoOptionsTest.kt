fb6a64d97bf886a583988dd725ce1d6fe75be1b0
kanunal99-jpg <kanunal99@gmail.com>
2026-09-08 09:13:23 +0300

    test: make ferry option assertions non-throwing

--- core/src/test/kotlin/com/haritalar/core/navigation/ValhallaAutoOptionsTest.kt
+++ core/src/test/kotlin/com/haritalar/core/navigation/ValhallaAutoOptionsTest.kt
@@ -6,8 +6,20 @@ import kotlin.test.assertEquals
 class ValhallaAutoOptionsTest {
     @Test
     fun mapsFerryPreferencesToValhallaWillingness() {
-        assertEquals(0.5, ValhallaAutoOptions.from(RouteTransportPreference.ALLOW_FERRY).getDouble("use_ferry"))
-        assertEquals(0.0, ValhallaAutoOptions.from(RouteTransportPreference.AVOID_FERRY).getDouble("use_ferry"))
-        assertEquals(1.0, ValhallaAutoOptions.from(RouteTransportPreference.PREFER_FERRY).getDouble("use_ferry"))
+        assertEquals(
+            0.5,
+            ValhallaAutoOptions.from(RouteTransportPreference.ALLOW_FERRY)
+                .optDouble("use_ferry", Double.NaN),
+        )
+        assertEquals(
+            0.0,
+            ValhallaAutoOptions.from(RouteTransportPreference.AVOID_FERRY)
+                .optDouble("use_ferry", Double.NaN),
+        )
+        assertEquals(
+            1.0,
+            ValhallaAutoOptions.from(RouteTransportPreference.PREFER_FERRY)
+                .optDouble("use_ferry", Double.NaN),
+        )
     }
 }
