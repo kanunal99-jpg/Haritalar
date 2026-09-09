e68cf4ddcdfd295fac2467e01f403dd485b1f138
kanunal99-jpg <kanunal99@gmail.com>
2026-09-09 15:02:56 +0300

    test(android): use explicit AndroidJUnit4 smoke runner

--- app/src/androidTest/java/com/haritalar/app/MainActivitySmokeTest.kt
+++ app/src/androidTest/java/com/haritalar/app/MainActivitySmokeTest.kt
@@ -1,5 +1,6 @@
 package com.haritalar.app
 
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
@@ -7,7 +8,9 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class)
 class MainActivitySmokeTest {
     @get:Rule
     val activityRule = ActivityScenarioRule(MainActivity::class.java)
