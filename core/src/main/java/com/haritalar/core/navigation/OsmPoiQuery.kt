920e6390d8c8572548551eb1fb9e7e309aa75e50
kanunal99-jpg <kanunal99@gmail.com>
2026-09-10 17:55:55 +0300

    feat(poi): expand live safety and map landmark queries

--- core/src/main/java/com/haritalar/core/navigation/OsmPoiQuery.kt
+++ core/src/main/java/com/haritalar/core/navigation/OsmPoiQuery.kt
@@ -43,15 +43,15 @@ object OsmPoiQuery {
         val query = scoped ?: """
               nwr[amenity~"^(restaurant|cafe|pharmacy|hospital|school|parking|fuel|bank|atm|charging_station|fast_food|marketplace|rest_area|townhall|courthouse|police|fire_station|post_office|library|community_centre)$"]($box);
               nwr[shop~"^(supermarket|convenience|mall|department_store|bakery|butcher|clothes|electronics|hardware|furniture)$"]($box);
-              nwr[highway="rest_area"]($box);
-              nwr[highway="traffic_signals"]($box);
-              nwr[highway="speed_camera"]($box);
+              nwr[highway~"^(rest_area|traffic_signals|speed_camera|crossing)$"]($box);
               nwr[enforcement~"^(maxspeed|average_speed)$"]($box);
               nwr[railway~"^(station|halt|tram_stop|subway_entrance)$"]($box);
               nwr[route="tram"]($box);
               nwr[office~"^(government|administrative)$"]($box);
               nwr[government]($box);
-              nwr[leisure="park"]($box);
+              nwr[leisure~"^(park|nature_reserve)$"]($box);
+              nwr[landuse="forest"]($box);
+              nwr[natural="wood"]($box);
               nwr[tourism~"^(hotel|attraction|museum|viewpoint)$"]($box);
               nwr[amenity="place_of_worship"]($box);
               nwr[public_transport]($box);
@@ -70,26 +70,28 @@ object OsmPoiQuery {
             nwr[shop~"^(supermarket|convenience|mall|department_store)$"]($box);
             nwr[amenity="marketplace"]($box);
         """.trimIndent()
-        NavigationPoiCategory.FUEL -> "nwr[amenity=\"fuel\"]($box);"
-        NavigationPoiCategory.PLACE_OF_WORSHIP -> "nwr[amenity=\"place_of_worship\"]($box);"
-        NavigationPoiCategory.PARKING -> "nwr[amenity=\"parking\"]($box);"
-        NavigationPoiCategory.RESTAURANT -> "nwr[amenity~\"^(restaurant|fast_food)$\"]($box);"
-        NavigationPoiCategory.CAFE -> "nwr[amenity=\"cafe\"]($box);"
-        NavigationPoiCategory.PHARMACY -> "nwr[amenity=\"pharmacy\"]($box);"
-        NavigationPoiCategory.HOSPITAL -> "nwr[amenity=\"hospital\"]($box);"
-        NavigationPoiCategory.SCHOOL -> "nwr[amenity=\"school\"]($box);"
-        NavigationPoiCategory.CHARGING_STATION -> "nwr[amenity=\"charging_station\"]($box);"
-        NavigationPoiCategory.TRAFFIC_SIGNAL -> "nwr[highway=\"traffic_signals\"]($box);"
+        NavigationPoiCategory.FUEL -> "nwr[amenity="fuel"]($box);"
+        NavigationPoiCategory.PLACE_OF_WORSHIP -> "nwr[amenity="place_of_worship"]($box);"
+        NavigationPoiCategory.PARKING -> "nwr[amenity="parking"]($box);"
+        NavigationPoiCategory.RESTAURANT -> "nwr[amenity~"^(restaurant|fast_food)$"]($box);"
+        NavigationPoiCategory.CAFE -> "nwr[amenity="cafe"]($box);"
+        NavigationPoiCategory.PHARMACY -> "nwr[amenity="pharmacy"]($box);"
+        NavigationPoiCategory.HOSPITAL -> "nwr[amenity="hospital"]($box);"
+        NavigationPoiCategory.SCHOOL -> "nwr[amenity="school"]($box);"
+        NavigationPoiCategory.CHARGING_STATION -> "nwr[amenity="charging_station"]($box);"
+        NavigationPoiCategory.TRAFFIC_SIGNAL -> "nwr[highway="traffic_signals"]($box);"
         NavigationPoiCategory.SPEED_CAMERA -> """
             nwr[highway="speed_camera"]($box);
             nwr[enforcement~"^(maxspeed|average_speed)$"]($box);
         """.trimIndent()
-        NavigationPoiCategory.TRAM -> "nwr[railway=\"tram_stop\"]($box); nwr[route=\"tram\"]($box);"
-        NavigationPoiCategory.RAILWAY -> "nwr[railway~\"^(station|halt|subway_entrance)$\"]($box);"
+        NavigationPoiCategory.PEDESTRIAN_CROSSING -> "nwr[highway="crossing"]($box);"
+        NavigationPoiCategory.TRAM -> "nwr[railway="tram_stop"]($box); nwr[route="tram"]($box);"
+        NavigationPoiCategory.RAILWAY -> "nwr[railway~"^(station|halt|subway_entrance)$"]($box);"
         NavigationPoiCategory.TRANSIT -> "nwr[public_transport]($box);"
-        NavigationPoiCategory.PARK -> "nwr[leisure=\"park\"]($box);"
-        NavigationPoiCategory.HOTEL -> "nwr[tourism=\"hotel\"]($box);"
-        NavigationPoiCategory.TOURISM -> "nwr[tourism~\"^(attraction|museum|viewpoint)$\"]($box);"
+        NavigationPoiCategory.PARK -> "nwr[leisure="park"]($box);"
+        NavigationPoiCategory.FOREST -> "nwr[landuse="forest"]($box); nwr[natural="wood"]($box); nwr[leisure="nature_reserve"]($box);"
+        NavigationPoiCategory.HOTEL -> "nwr[tourism="hotel"]($box);"
+        NavigationPoiCategory.TOURISM -> "nwr[tourism~"^(attraction|museum|viewpoint)$"]($box);"
         NavigationPoiCategory.PUBLIC_INSTITUTION -> """
             nwr[amenity~"^(townhall|courthouse|police|fire_station|post_office|library|community_centre)$"]($box);
             nwr[office~"^(government|administrative)$"]($box);
